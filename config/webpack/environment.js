cf5ea29b40e54e96679d23d26db30467bdf1d98d
岡本能知 <daichiiiii317@icloud.com>
2024-07-19 05:28:33 +0900

    package_json

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,13 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 
+// エントリーポイントの設定を追加
+environment.config.merge({
+  entry: {
+    application: './app/javascript/packs/application.js'
+  }
+})
+
 environment.plugins.prepend('Provide', 
   new webpack.ProvidePlugin({
     $: 'jquery',
